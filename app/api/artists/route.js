fba1263c34fee543e2382e43cfa739cc055a1c1b
Omar Camara <117489682+Username273183@users.noreply.github.com>
2024-05-10 20:20:51 -0400

    05/10/2024

--- app/api/artists/route.js
+++ app/api/artists/route.js
@@ -3,49 +3,47 @@ import { getAccessToken } from "../../../lib/spotify";
 export async function GET() {
   const token = await getAccessToken();
 
-  //const taylorSwift = "06HL4z0CvFAxyc27GXpf02";
-
   const artists = [
     {
       name: "Taylor Swift",
       spotify_id: "06HL4z0CvFAxyc27GXpf02",
     },
-    {
-      name: "Beyoncé",
-      spotify_id: "6vWDO969PvNqNYHIOW5v0m",
-    },
-    {
-      name: "Ariana Grande",
-      spotify_id: "66CXWjxzNUsdJxJ2JdwvnR",
-    },
-    {
-      name: "Thelonius Monk",
-      spotify_id: "4PDpGtF16XpqvXxsrFwQnN",
-    },
-    {
-      name: "Dizzy Gillespie",
-      spotify_id: "5RzjqfPS0Bu4bUMkyNNDpn",
-    },
-    {
-      name: "Charlie Parker",
-      spotify_id: "4Ww5mwS7BWYjoZTUIrMHfC",
-    },
-    {
-      name: "Sonny Rollins",
-      spotify_id: "1VEzN9lxvG6KPR3QQGsebR",
-    },
-    {
-      name: "Dexter Gordon",
-      spotify_id: "3NUsiT2JSyaWAnWaXxDzhQ",
-    },
-    {
-      name: "Kenny Burrell",
-      spotify_id: "1sdyFmN4bVOcuFDpTVsxBB",
-    },
-    {
-      name: "Wes Montgomery",
-      spotify_id: "03YhcM6fxypfwckPCQV8pQ",
-    },
+    // {
+    //   name: "Beyoncé",
+    //   spotify_id: "6vWDO969PvNqNYHIOW5v0m",
+    // },
+    // {
+    //   name: "Ariana Grande",
+    //   spotify_id: "66CXWjxzNUsdJxJ2JdwvnR",
+    // },
+    // {
+    //   name: "Thelonius Monk",
+    //   spotify_id: "4PDpGtF16XpqvXxsrFwQnN",
+    // },
+    // {
+    //   name: "Dizzy Gillespie",
+    //   spotify_id: "5RzjqfPS0Bu4bUMkyNNDpn",
+    // },
+    // {
+    //   name: "Charlie Parker",
+    //   spotify_id: "4Ww5mwS7BWYjoZTUIrMHfC",
+    // },
+    // {
+    //   name: "Sonny Rollins",
+    //   spotify_id: "1VEzN9lxvG6KPR3QQGsebR",
+    // },
+    // {
+    //   name: "Dexter Gordon",
+    //   spotify_id: "3NUsiT2JSyaWAnWaXxDzhQ",
+    // },
+    // {
+    //   name: "Kenny Burrell",
+    //   spotify_id: "1sdyFmN4bVOcuFDpTVsxBB",
+    // },
+    // {
+    //   name: "Wes Montgomery",
+    //   spotify_id: "03YhcM6fxypfwckPCQV8pQ",
+    // },
   ];
 
   const artistsIds = artists.map((artist) => artist.spotify_id);
